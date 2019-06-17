ebfa75c62ff6e30ca79335f47132eee32b5b0536
cis101 <cis101@secs.iub.edu.bd>
2019-06-17 09:40:27 +0600

    Completed

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 /* JavaScript code here */
-
-console.log("External Javascript")
+console.log("External Javascript");
 
 // Task 2: Array Sum
 
@@ -8,65 +7,67 @@ function sum(arr){
     var sum = 0;
     arr.forEach(function(item, index){
         sum = sum + item;
-})
-return sum;
+    })
+    return sum;
 }
 
-console.log(sum([1, 2, 3, 4]))
+console.log("Array Sum: " + sum([5, 10, 15, 20]));
 
-// Task 3: Email validation
-/* var validationEmail = function(email) */
-function validationEmail(email){
+// Task 3: Email Validation with reg expression
+
+function validateEmail(email){
+// Regular Expression to validate proper email address
+// /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
 }
 
-console.log(validationEmail("ssssssdsdas"))
-console.log(validationEmail("sabbirhossainabed@gmail.com"))
-
-/*object*/
+console.log("Email: yellow.com is " + validateEmail("yellow.com"));
+console.log("Email: 1510927@iub.edu.bd is " + validateEmail("1510927@iub.edu.bd"));
+console.log("Email: rifatmahmud196@gmail.com is " + validateEmail("rifatmahmud196@gmail.com"));
 
 var company = {
-    name: 'Google', 
+    name: "Google", 
     age: 19, 
     ceo: {
-    first_name: 'Sundar', 
-    last_name: 'Pichai'
+    first_name: "Sundar", 
+    last_name: "Pichai"
     }
-};
-console.log(company)
-company.updateAge = function(newAge){
-    this.age = newAge;
+    };
+
+    company.updateAge = function (newAge) {
+        this.age = newAge;
+    }
+
+    company.updateAge(23);
+    console.log(company["age"])
+
+    //Task 4: Access Object status at a specified index of array
+
+    var library = [ 
+        {
+            author: 'Bill Gates',
+            title: 'The Road Ahead',
+            readingStatus: true
+        },
+        {
+            author: 'Steve Jobs',
+            title: 'Walter Isaacson',
+            readingStatus: true
+        },
+        {
+            author: 'Suzanne Collins',
+            title:  'Mockingjay: The Final Book of The Hunger Games', 
+            readingStatus: false
+        }];
+
+        
+library.readingStatus = function (index) {
+    return library[index].readingStatus;
 }
 
-company.updateAge(23);
-console.log(company["age"]);
+console.log(library.readingStatus(2))
 
- 
-/*Task 4: Given the following array of objects, write a function that prints the status at a given index:*/
-
-var library = [ 
-    {
-        author: 'Bill Gates',
-        title: 'The Road Ahead',
-        readingStatus: true
-    },
-    {
-        author: 'Steve Jobs',
-        title: 'Walter Isaacson',
-        readingStatus: true
-    },
-    {
-        author: 'Suzanne Collins',
-        title:  'Mockingjay: The Final Book of The Hunger Games', 
-        readingStatus: false
-    }];
-
-var status = function(index, prop){
-    return library[index][prop]
-} 
-console.log(status(2, 'author'));
-
-/*Task 5*/
+//Task 5a: Add a new item
 
 var cart = [ 
     {
@@ -84,31 +85,63 @@ var cart = [
         price: 65.99,
         quantity: 2
     }];
-    
-    // 5a - Add a new item
-    function addItem(newItem){
-        cart.push(newItem);
-    }
-    addItem({name: 'Socks',
-    price: 65.99,
-    quantity: 2});
-    console.log(cart)
 
-    // 5b - sort by a given property
-    function sortCart(prop){
-        cart.sort(function(a, b){
-            return a[prop] > b[prop]
-        })
-    }
-    sortCart('name')
-    console.log(cart)
+function addItem(newItem){
+    cart.push(newItem);
+}
 
-    //5c - find item by name
-    function findByName(name){
+addItem({
+    name: 'Pants',
+    price: 29.99,
+    quantity: 10
+})
 
-    }
+addItem({
+    name: 'Shoes',
+    price: 49.99,
+    quantity: 15
+})
 
-    //5d - get total cost
-    function getTotalCost(){
+console.log(cart)
 
-    }
+//Task 5b: Sort by a given property
+
+function sortCart(prop){
+    cart.sort(function(a, b){
+        if (a[prop] < b[prop]){
+            return -1;
+        }
+        if (a[prop] > b[prop]){
+            return 1;
+        }
+        return 0;
+    })
+}
+
+sortCart('quantity')
+console.log(cart)
+
+//Task 5c: Find Item by name
+
+findByName = function(name){
+var newArr =  cart.filter(function(item) {
+    return item.name == name;
+});
+
+console.log(newArr)
+}
+
+findByName("Shoes")
+
+//Task 5d: Get total cost
+
+var totalCost = null
+
+function getTotalCost() {
+    cart.forEach(function(value, index, arry){
+        totalCost += value.price;
+       });
+}
+
+getTotalCost()
+console.log(totalCost)
