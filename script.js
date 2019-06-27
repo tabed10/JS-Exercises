4fe149e7bae69ea9734149cae6b1f03f5fcf7c7d
tabed10 <sabbirhossainabed@gmail.com>
2019-06-28 02:03:53 +0600

    corrections

--- script.js
+++ script.js
@@ -60,12 +60,14 @@ var library = [
         title:  'Mockingjay: The Final Book of The Hunger Games', 
         readingStatus: false
     }];
+//function getReadingStatus(index){
+function getProp(index, prop){
+//return library[index].readingStatus
+return library[index][prop]
 
-var status = function(index, prop){
-    return library[index][prop]
-} 
-console.log(status(2, 'author'));
-
+}   
+//console.log(getReadingStatus(1)) 
+console.log(getProp(1, 'author'))
 /*Task 5*/
 
 var cart = [ 
@@ -96,19 +98,34 @@ var cart = [
 
     // 5b - sort by a given property
     function sortCart(prop){
-        cart.sort(function(a, b){
-            return a[prop] > b[prop]
-        })
+        cart.sort(function(a,b){
+            if ( a[prop] < b[prop] ){
+                return 1;
+              }
+              if ( a[prop] > b[prop]){
+                return 1;
+              }
+              return 0;
+        }) 
     }
-    sortCart('name')
-    console.log(cart)
+        sortCart('name')
+        console.log(cart)
 
     //5c - find item by name
-    function findByName(name){
-
+    findByName = function(name){
+        var myArr =  cart.filter(function(item) {
+            return item.name == name;
+    });
+    console.log(myArr)
     }
+    findByName("Shoes")
 
     //5d - get total cost
     function getTotalCost(){
-
+        var totalCost = 0;
+        for (var i in cart){
+            totalCost += cart[i].price;
+        }
+        return totalCost;
     }
+     console.log(getTotalCost());
